15478928adce88102763ce677e34201407b050a9
pomo0508 <forever-career@hotmail.co.jp>
2024-05-19 22:52:12 +0900

    testコードをお試し追加

--- tests/utils/test_chat.py
+++ tests/utils/test_chat.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from dotenv import load_dotenv
 from langchain_core.language_models import FakeMessagesListChatModel
 from langchain_core.messages import AIMessage, BaseMessage
@@ -7,7 +9,14 @@ from utils.chat import GenerateChatResponseChain
 # .envファイルを読み込む
 load_dotenv()
 
-def test_invoke() -> None:
+
+@patch("utils.chat.fetch_recent_chat_messages")
+def test_invoke(mock_fetch_recent_chat_messages) -> None:
+    # fetch_recent_chat_messagesの戻り値をモック化
+    mock_fetch_recent_chat_messages.return_value = [
+        ("human", "こんにちは"),
+        ("ai", "こんにちは！元気ですか？"),
+    ]
     response = """こんにちは！ 😊
 
 お元気ですか？何かお手伝いできることはありますか？
