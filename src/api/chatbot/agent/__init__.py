6436da050152989321774c0591b4939fafd4f9e5
Tomodo1773 <forever-career@hotmail.co.jp>
2024-12-26 14:39:55 +0900

    supervisor型に変更（途中）

--- src/api/chatbot/agent/__init__.py
+++ src/api/chatbot/agent/__init__.py
@@ -1,7 +1,7 @@
 import datetime
 import getpass
 import os
-from typing import Annotated
+from typing import Annotated, Literal
 
 import pytz
 from chatbot.agent.prompt import get_character_prompt
@@ -11,10 +11,14 @@ from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_openai import ChatOpenAI
-from langgraph.graph import StateGraph
+from langgraph.graph import END, START, StateGraph
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.types import Command
+from pydantic import BaseModel, Field
 from typing_extensions import TypedDict
+from langchain_core.prompts import PromptTemplate
+
 
 # ############################################
 # 事前準備
@@ -51,6 +55,7 @@ class ChatbotAgent:
         self.tools = [TavilySearchResults(max_results=3), firecrawl_search, azure_ai_search]
         self.userid = userid
         graph_builder = StateGraph(State)
+        graph_builder.add_node("supervisor", self._supervisor_node)
         graph_builder.add_node("chatbot", self._chatbot_node)
         graph_builder.add_node("tools", self._tool_node)
         graph_builder.add_conditional_edges(
@@ -58,16 +63,58 @@ class ChatbotAgent:
             tools_condition,
         )
         graph_builder.add_edge("tools", "chatbot")
-        graph_builder.set_entry_point("chatbot")
+        graph_builder.add_edge(START, "supervisor")
         self.graph = graph_builder.compile()
+
+    def _supervisor_node(self, state: State) -> Command[Literal["agent_1", "agent_2", "__end__"]]:
+        # you can pass relevant parts of the state to the LLM (e.g., state["messages"])
+        # to determine which agent to call next. a common pattern is to call the model
+        # with a structured output (e.g. force it to return an output with a "next_agent" field)
+        # response = model.invoke(...)
+        # route to one of the agents or exit based on the supervisor's decision
+        # if the supervisor returns "__end__", the graph will finish execution
+
+        """
+        Determines whether the retrieved documents are relevant to the question.
+
+        Args:
+            state (messages): The current state
+
+        Returns:
+            str: A decision for whether the documents are relevant or not
+        """
+
+        print("---CHECK RELEVANCE---")
+
+        members = ["web_searcher", "diary_searcher"]
+
+        system_prompt = (
+            "You are a supervisor tasked with managing a conversation between the"
+            f" following workers: {members}. Given the following user request,"
+            " respond with the worker to act next. Each worker will perform a"
+            " task and respond with their results and status. When finished,"
+            " respond with FINISH."
+        )
+
+        class Router(TypedDict):
+            """Worker to route to next. If no workers needed, route to FINISH."""
+            next: Literal["web_searcher", "diary_searcher", "FINISH"]
+
+        llm = ChatAnthropic(model="claude-3-5-sonnet-latest")
+        messages = [
+            {"role": "system", "content": system_prompt},
+        ] + state["messages"]
+        response = llm.with_structured_output(Router).invoke(messages)
+        goto = response["next"]
+        if goto == "FINISH":
+            goto = END
+
+        return Command(goto=goto)
 
     def _chatbot_node(self, state: State):
         # llm = ChatOpenAI(model="gpt-4o")
-        # llm = ChatGoogleGenerativeAI(
-        #     model="gemini-1.5-pro-latest",
-        #     temperature=0.2,
-        # )
-        llm = ChatAnthropic(model="claude-3-5-sonnet-latest")
+        llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest")
+        # llm = ChatAnthropic(model="claude-3-5-sonnet-latest")
         llm_with_tools = llm.bind_tools(self.tools)
         prompt = get_character_prompt(self.userid)
         chatbot_chain = prompt | llm_with_tools
@@ -114,3 +161,4 @@ if __name__ == "__main__":
         for event in agent_graph.stream(history):
             for value in event.values():
                 print("Assistant:", value["messages"][-1].content)
+                history.append({"type": "assistant", "content": value["messages"][-1].content})
