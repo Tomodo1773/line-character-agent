9775020099d09235825b39e867368164e4e9167d
Tomodo1773 <forever-career@hotmail.co.jp>
2025-07-12 21:10:20 +0900

    feat: 環境変数の読み込みと除外ファイルチェックを追加

--- src/func/function_app.py
+++ src/func/function_app.py
@@ -1,11 +1,15 @@
 import datetime
+import os
 
 import azure.functions as func
+from dotenv import load_dotenv
 
 from cosmosdb import CosmosDBUploader
 from get_google_drive import GoogleDriveHandler
-import os
 from logger import logger
+
+# 環境変数を.envファイルから読み込み
+load_dotenv()
 
 app = func.FunctionApp()
 
@@ -17,6 +21,9 @@ def timer_trigger(myTimer: func.TimerRequest) -> None:
 
 
 def upload_recent_diaries(span_days: int = 1):
+    # 除外するファイル名のリスト
+    excluded_files = {"dictionary.md", "digest.md", "profile.md"}
+
     # Get the list of files from Google Drive
     drive_handler = GoogleDriveHandler()
     files = drive_handler.list()
@@ -32,6 +39,11 @@ def upload_recent_diaries(span_days: int = 1):
     # Iterate over the files and check their modified time
     documents = []
     for file in files:
+        # 除外ファイルチェック
+        if file["name"] in excluded_files:
+            logger.info(f"File {file['name']} is excluded from upload.")
+            continue
+
         modified_time = datetime.datetime.strptime(file["modifiedTime"], "%Y-%m-%dT%H:%M:%S.%fZ")
 
         # Check if the file was modified within the last day
